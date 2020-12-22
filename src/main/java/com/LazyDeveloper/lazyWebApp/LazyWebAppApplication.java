80937af86ef5c75d51f6b439bd3ecd1f90028a95
Petre Vane <petre.vane@gmail.com>
2020-12-22 12:26:34 +0100

    first commit

---
+++ src/main/java/com/LazyDeveloper/lazyWebApp/LazyWebAppApplication.java
@@ -0,0 +1,14 @@
+package com.LazyDeveloper.lazyWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazyWebAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(LazyWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LazyDeveloper/lazyWebApp/LazyWebAppApplicationTests.java
@@ -0,0 +1,14 @@
+package com.LazyDeveloper.lazyWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazyWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
